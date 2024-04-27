5ffc7517954733b679d6d7e063cdecb982362ffc
wilsonhiperbanco <wilson.pinheiro@hiperbanco.com.br>
2024-04-26 22:25:53 -0300

    infra: added config semantic release

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: ['main'], // Ramo onde as versões serão geradas
+  plugins: [
+    '@semantic-release/commit-analyzer', // Analisa os commits e determina o tipo de versão
+    '@semantic-release/release-notes-generator', // Gera notas de lançamento com base nos commits
+    '@semantic-release/changelog', // Gera um arquivo CHANGELOG.md
+    '@semantic-release/npm', // Atualiza o número da versão no package.json
+    '@semantic-release/git', // Commita os arquivos alterados e cria uma tag de versão no Git
+  ],
+};
