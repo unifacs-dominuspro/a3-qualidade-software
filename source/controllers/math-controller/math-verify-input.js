3f39b9426f1a84110c27a325d0ff042b9918e2db
wilson6g <wilsonpinheirodev@gmail.com>
2024-05-01 17:34:00 -0300

    refactor: updated regeex for get only numbers and math expressions

--- source/controllers/math-controller/math-verify-input.js
+++ source/controllers/math-controller/math-verify-input.js
@@ -1,21 +1,22 @@
 const Joi = require('joi');
 
 const performsOperation = Joi.object().keys({
-  numbers: Joi.array().items(Joi.string().regex(/(\d?[+*/%√.-])/).messages(
+  numbers: Joi.array().items(Joi.string().regex(/^\d+(\.\d+)?(?:[+\-*\/%√]|(?:\.\d+)?-)?\d+(\.\d+)?$/).messages(
     {
       "string.pattern.base": "Os valores só podem ser números com operações matemáticas"
     }
   ))
 }).required().messages({
   'string.base': `Os valores devem ser do tipo número`,
-  'string.empty': `Os valores não podem ser vazio`,
+  'string.empty': `Os valores não podem ser vazios`,
   'any.required': `Os valores são obrigatórios`
-})
+});
 
 const validateFieldNumberForBasicOperations = (request, response) => {
   const validation = performsOperation.validate(request.body);
 
   const { error } = validation;
+
   if (error) {
     return response.status(422).json({
       message: error.details[0].message,
